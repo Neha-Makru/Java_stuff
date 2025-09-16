491fbd139fe4355126048a0c61b66e9d2cc34083
Neha Makru <137904719+Neha-Makru@users.noreply.github.com>
2025-09-16 12:53:12 +0530

    Create 2D_Array_output.java

---
+++ 2D_Array_output.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3}, {3, 4, 6}, {7, 8, 9}};
+        for (int [] e: a){
+            System.out.println(Arrays.toString(e));
+        }
+    }
+}
