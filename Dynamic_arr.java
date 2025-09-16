d61b8b50e2eef2292eb5750cc0c329d1befe9fe1
Neha Makru <137904719+Neha-Makru@users.noreply.github.com>
2025-09-16 13:10:03 +0530

    Create Dynamic_arr.java

---
+++ Dynamic_arr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <String> names = new ArrayList<>(1);
+        names.add("Neha");
+        names.add("Pino");
+        names.add("Pikachu");
+        System.out.println(names);
+    }
+}
