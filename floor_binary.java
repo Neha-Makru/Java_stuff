29f3e4bb1aa8049cae121f314bfee2ec69e96403
Neha Makru <137904719+Neha-Makru@users.noreply.github.com>
2025-09-17 17:56:53 +0530

    Update floor_binary.java

--- floor_binary.java
+++ floor_binary.java
@@ -3,33 +3,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         int[] a = {2, 3, 5, 9, 14, 16, 18};
-        int target = 4;
-        floor(a, target);
+        int target = 7;
+        int res = floor(a, target);
+        System.out.println("The floor value is: " + res);
     }
+    static int floor(int [] arr, int target){
+        int start = 0, end = arr.length-1;
+        while (start <= end){
+            int mid = (start + (end - start)/2);
 
-    static void floor(int[] arr, int target) {
-        int start = 0, end = arr.length - 1;
-
-        if (target < arr[start]) {
-            System.out.println("No floor value found.");
-            return;
-        }
-
-        int floorValue = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (arr[mid] == target) {
-                System.out.println("The floor is: " + arr[mid]);
-                return;
-            } else if (arr[mid] > target) {
-                end = mid - 1;
-            } else {
-                floorValue = arr[mid];
+            if (arr[mid]== target){
+                return arr[mid];
+            }
+            else if (arr[mid] < target) {
                 start = mid + 1;
             }
+            else end = mid -1;
         }
-        System.out.println("The floor is: " + floorValue);
+        return arr[end];
     }
 }
